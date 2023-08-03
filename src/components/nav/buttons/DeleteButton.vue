4d039ce33bd0f15e50ffcc75cdd6e8bd039a16db
lucasyoungers <lucas005xl@gmail.com>
2023-08-03 01:28:15 -0700

    retooled deck clear button to not navigate away when clicked

--- src/components/nav/buttons/DeleteButton.vue
+++ src/components/nav/buttons/DeleteButton.vue
@@ -1,25 +1,18 @@
 <template>
-    <Button @click="deleteDeck" icon="delete" rounded="right" :disabled="!deckStore.count" />
+    <Button @click="deckStore.clear()" icon="delete" rounded="right" :disabled="!deckStore.count" />
 </template>
 
 <script>
     import Button from "@/components/nav/buttons/Button"
     import useDeckStore from "@/stores/deck"
-import { useRouter } from 'vue-router'
 
     export default {
         name: "DeleteButton",
         components: { Button },
         setup() {
             const deckStore = useDeckStore()
-            const router = useRouter()
 
-            const deleteDeck = () => {
-                deckStore.clear()
-                router.push({ name: "Home" })
-            }
-
-            return { deckStore, deleteDeck }
+            return { deckStore }
         }
     }
 </script>
