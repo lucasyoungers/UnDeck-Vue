7e022179496defb37abc90ef8e213f6d9e650e69
lucasyoungers <lucas005xl@gmail.com>
2023-08-07 20:58:54 -0700

    added loading icon when loading more search cards

--- src/composables/getSearchCards.js
+++ src/composables/getSearchCards.js
@@ -6,9 +6,13 @@ const path = process.env.NODE_ENV === "development" ? "http://localhost:3080" : 
 
 const getSearchCards = query => {
     const errorStore = useErrorStore()
+
     const searchCards = ref([])
+    const onLastPage = ref(false)
     const searchError = ref(null)
+
     let page = 1
+
     const loadSearchCards = async () => {
         try {
             errorStore.setCardsNotFound(false)
@@ -18,6 +22,9 @@ const getSearchCards = query => {
             const res = await fetch(path + `/api/search?${queryParams.toString()}`)
             await statusCheck(res)
             const data = await res.json()
+            if (data.page * data.pageSize >= data.totalCount) {
+                onLastPage.value = true
+            }
             if (data.count == 0) {
                 errorStore.setCardsNotFound(true)
             }
@@ -27,7 +34,7 @@ const getSearchCards = query => {
             searchError.value = err.message
         }
     }
-    return { searchCards, searchError, loadSearchCards }
+    return { searchCards, onLastPage, searchError, loadSearchCards }
 }
 
 export default getSearchCards
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,42 +1,56 @@
 <template>
     <section class="search">
         <Cards :cards="searchCards" />
+        <CirclesToRhombusesSpinner v-if="searching" class="spinner" color="#3b165c" :circle-size="30" />
     </section>
 </template>
 
 <script>
     import Cards from "@/components/Cards"
+    import { CirclesToRhombusesSpinner } from "epic-spinners"
     import getSearchCards from "@/composables/getSearchCards"
-    import useErrorStore from "@/stores/error"
-    import { onMounted, onUnmounted } from "vue"
+    import { onMounted, onUnmounted, ref } from "vue"
     import { useRoute } from "vue-router"
 
     export default {
         name: "Search",
-        components: { Cards },
+        components: { Cards, CirclesToRhombusesSpinner },
         setup() {
             const { query } = useRoute()
 
-            const { searchCards, loadSearchCards } = getSearchCards(query)
+            const { searchCards, onLastPage, loadSearchCards } = getSearchCards(query)
             loadSearchCards()
 
-            const errorStore = useErrorStore()
+            const searching = ref(false)
 
             const loadMoreSearchCards = async () => {
-                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                    window.removeEventListener("scroll", loadMoreSearchCards)
-                    await loadSearchCards()
-                    window.addEventListener("scroll", loadMoreSearchCards)
+                // don't try to load more until first batch is loaded
+                if (searchCards.value.length === 0) return
 
-                    if (errorStore.cardsNotFound) {
-                        window.removeEventListener("scroll", loadMoreSearchCards)
+                // if the user has scrolled to the bottom, fetch more cards, assuming there are any
+                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                    // don't allow loading more until this batch is loaded
+                    window.removeEventListener("scroll", loadMoreSearchCards)
+
+                    searching.value = true
+
+                    // hack to scroll to loading dots after they appear
+                    setTimeout(() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }), 0);
+
+                    await loadSearchCards()
+
+                    searching.value = false
+
+                    // only re-enable getting more cards if there are any left
+                    if (!onLastPage.value) {
+                        window.addEventListener("scroll", loadMoreSearchCards)
                     }
                 }
             }
             onMounted(() => window.addEventListener("scroll", loadMoreSearchCards))
             onUnmounted(() => window.removeEventListener("scroll", loadMoreSearchCards))
 
-            return { searchCards }
+            return { searchCards, searching }
         }
     }
 </script>
@@ -44,5 +58,9 @@
 <style scoped>
     .search {
         margin: 0 var(--gap) var(--gap);
+    }
+
+    .spinner {
+        margin: 2rem auto;
     }
 </style>
